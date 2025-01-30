b9912814d8b20b3f834920405b84463c729356a5
MattGaviria <cmateogaviria@gmail.com>
2025-01-30 09:53:19 -0500

    Week 4 update

--- COMP2139/Lab1/Controllers/ProjectController.cs
+++ COMP2139/Lab1/Controllers/ProjectController.cs
@@ -1,22 +1,34 @@
-﻿using Lab1.Models;
+﻿using Lab1.Data;
+using Lab1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab1.Controllers;
 
 public class ProjectController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     /// <summary>
-    /// Index action will retrive a listing of projects (datanase)
+    /// Index action will retrieve a listing of projects (database)
     /// </summary>
     
     [HttpGet]
     public IActionResult Index()
     {
-        var projects = new List<Project>
-        {
-            new Project { ProjectId = 1, Name = "Project1", Description = " First Project 1" },
-        };
+        // Retrieve all projects from the database
+        var projects=_context.Projects.ToList();
         return View(projects);
+        
+       // var projects = new List<Project>
+        //{
+         //   new Project { ProjectId = 1, Name = "Project1", Description = " First Project 1" },
+       // };
+        //return View(projects);
     }
 
     [HttpGet]
@@ -26,18 +38,97 @@ public class ProjectController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult Create(Project project)
     {
         //Persist new project to the database
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+            _context.Projects.Add(project); // add new project to database
+            _context.SaveChanges(); //persists the changes to database
+            return RedirectToAction("Index");
+        }
+        
+        return View("Index");
     }
     
     //CRUD CREATE - READ -UPDATE - DELETE
-
+    
+    [HttpGet]
     public IActionResult Details(int id)
     {
-        // Retrive project form database
-        var project = new Project {ProjectId = id, Name = "Project 1", Description = " First Project" };
+        // retrieves the project with the specified id or returns null if not found
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();
+            
+        }
+        return View(project);
+        // Retrieve project form database
+        // var project = new Project {ProjectId = id, Name = "Project 1", Description = " First Project" };
+        //return View(project);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var project = _context.Projects.Find(id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return View(project);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, [Bind("ProjectID, name, Description")] Project project)
+    {
+        if (id != project.ProjectId)
+        {
+            return NotFound(); 
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Projects.Update(project);//update the project
+                _context.SaveChanges();// commit changes
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(project.ProjectId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        return View(project);
+    }
+
+    private bool ProjectExists(int id)
+    {
+        return _context.Projects.Any(e => e.ProjectId == id);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        //Retrieves the project with the specified id or return null if not found
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound(); 
+            
+        }
         return View(project);
     }
 }
--- COMP2139/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ COMP2139/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa111df3bb84aa2b40fd81c660d3bf21e25642ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ba375e783feebe944b4a91d76f5e5dcbed8117")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
