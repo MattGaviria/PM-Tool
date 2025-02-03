aae53fd25464d27d76029d99cbef0dc4eb8477b9
MattGaviria <cmateogaviria@gmail.com>
2025-02-03 09:50:25 -0500

    Monday Feb 3rd update

--- COMP2139/Lab1/Controllers/ProjectController.cs
+++ COMP2139/Lab1/Controllers/ProjectController.cs
@@ -131,4 +131,21 @@ public class ProjectController : Controller
         }
         return View(project);
     }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int ProjectId)
+
+    {
+        var project = _context.Projects.Find(ProjectId);
+        if (project != null)
+        {
+            _context.Projects.Remove(project);// remove project from database
+            _context.SaveChanges(); //commit
+            return RedirectToAction("Index");
+        }
+
+        return NotFound();
+    }
+    
 }
--- COMP2139/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ COMP2139/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ba375e783feebe944b4a91d76f5e5dcbed8117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9912814d8b20b3f834920405b84463c729356a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
